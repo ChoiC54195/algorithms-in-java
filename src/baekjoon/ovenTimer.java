f5386284af4b7e104e2fcb07ff123c459ae83703
ChoiC <choic54195@gmail.com>
2022-05-28 08:11:40 +0900

    20220528_백준알고리즘

---
+++ src/baekjoon/ovenTimer.java
@@ -0,0 +1,31 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class ovenTimer {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		
+		int hour = Integer.parseInt(st.nextToken());
+		int min = Integer.parseInt(st.nextToken());
+		
+		int time = Integer.parseInt(br.readLine());
+		
+		min += time;
+		if(min > 59) {
+			hour += (min/60);
+			min-=(min/60*60);
+		}
+		if(hour > 23) {
+			hour-=24;
+		}
+		System.out.println(hour+" "+min);
+		
+	}
+
+}
